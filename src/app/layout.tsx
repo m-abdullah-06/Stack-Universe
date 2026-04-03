0b7151e46cd8b72d06aadf260a176ab4b6c34c73
Muhammad Abdullah <abdullah.muhammad.xyz@gmail.com>
2026-04-03 23:33:58 +0500

    added insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
 import { NextAuthProvider } from "@/components/providers/NextAuthProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -28,6 +30,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="scanlines bg-space-black overflow-hidden font-mono">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZGTB120103"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZGTB120103');
+          `}
+        </Script>
         <NextAuthProvider>{children}</NextAuthProvider>
         <Analytics />
       </body>
